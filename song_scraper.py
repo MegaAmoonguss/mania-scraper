f3de7f2c9ca83988f3669d7cb41d4b60f101af41
Graham Preston <graham.preston@gmail.com>
2017-04-10 20:58:16 -0400

    Song scraping update

--- song_scraper.py
+++ song_scraper.py
@@ -8,9 +8,10 @@ conn = sqlite3.connect("songs.db")
 c = conn.cursor()
 
 try:
-    c.execute("SELECT * FROM songs")
+    c.execute("DROP TABLE songs")
 except sqlite3.OperationalError:
-    c.execute("CREATE TABLE songs (title text, version text, count real)")
+    pass
+c.execute("CREATE TABLE songs (title text, version text, count real)")
 
 # get user data
 res = requests.get("https://new.ppy.sh/u/5572803")
@@ -29,38 +30,19 @@ user_data = json.loads(str(tag)[47:-9].strip())
 # get song count info
 for i in range(100):
     title, version = (
-        user_data["allScoresBest"]["mania"][i]["beatmapset"]["title"],
-        user_data["allScoresBest"]["mania"][i]["beatmap"]["version"]
+        user_data["allScoresBest"]["mania"][i]["beatmapset"]["title"].replace("'", "").replace('"', ''),
+        user_data["allScoresBest"]["mania"][i]["beatmap"]["version"].replace("'", "").replace('"', '')
         )
-    # TODO: check if title + version is already an entry, if it is then increment it
-
-# TODO: move score scraping code to new file once song scraping is finished
-# get score data
-#===============================================================================
-# username, title, version, rating, score, accuracy = (
-#     user_data["username"],
-#     user_data["allScoresBest"]["mania"][0]["beatmapset"]["title"],
-#     user_data["allScoresBest"]["mania"][0]["beatmap"]["version"],
-#     "%.2f" % (user_data["allScoresBest"]["mania"][0]["beatmap"]["difficulty_rating"]),
-#     user_data["allScoresBest"]["mania"][0]["score"],
-#     "%.2f" % (user_data["allScoresBest"]["mania"][0]["accuracy"] * 100)
-#     )
-#===============================================================================
-
-# load scores to database
-#===============================================================================
-# conn = sqlite3.connect("mania_data.db")
-# c = conn.cursor()
-# 
-# try:
-#     c.execute("SELECT * FROM mania")
-# except sqlite3.OperationalError:
-#     c.execute("CREATE TABLE mania (username text, title text, version text, rating real, score real, accuracy real)")
-# 
-# c.execute(f"INSERT INTO mania VALUES ('{username}', '{title}', '{version}', {rating}, {score}, {accuracy})")
-# for row in c.execute("SELECT * FROM mania"):
-#     print(row)
-#     
-# conn.commit()
-# conn.close()
-#===============================================================================
+    
+    # add to database
+    entry = c.execute(f"SELECT * FROM songs WHERE title='{title}' AND version='{version}'")
+    if not entry.fetchall():
+        c.execute(f"INSERT INTO songs VALUES ('{title}', '{version}', 1)")
+    else:
+        c.execute(f"UPDATE songs SET count = count + 1 WHERE title='{title}' AND version='{version}'")
+    
+for row in c.execute("SELECT * FROM songs"):
+    print(row)
+    
+conn.commit()
+conn.close()
