ebda2b418e30a8c8a3e6438b99ac9ac0ef681b3c
Graham Preston <graham.preston@gmail.com>
2017-04-24 11:07:59 -0400

    Finish score scraper, push scores

---
+++ scripts/score_scraper.py
@@ -0,0 +1,48 @@
+import json
+
+song_list = [
+    ["Speedcore 300", "Extra"],
+    ["croiX", "GRAVITY"],
+    ["over the top", "Extra"],
+    ["Blastix Riotz", "Jinjin s INFINITE"],
+    ["HAELEQUIN", "INF"],
+    ["Bangin  Burst", "EXHAUST Lv.16"],
+    ["Verse IV", "INFINITE"],
+    ["Tokyo Teddy Bear", "SHD"],
+    ["Galaxy Collapse", "Cataclysmic Hypernova"],
+    ["M.A.M.A.", "SHD"],
+    ["Kakuzetsu Thanatos", "Isolation"],
+    ["G1ll35 d3 R415", "L45T C4LL"],
+    ["LegenD.", "KK s GRAVITY"],
+    ["C18H27NO3(extend)", "4K Capsaicin"],
+    ["BLACK or WHITE?", "DECADE vs. Usagi s INFINITE Lv.16"],
+    ["Lachryma<Re:Queen M>", "GRAVITY"],
+    ["Hesperides", "Master"],
+    ["Bokutachi no Tabi to Epilogue.[Long ver.]", "Final Voyage"],
+    ["G1ll35 d3 R415", "Shana s Extra"],
+    ["Space Time (Amane Hardcore Remix)", "Spy s 4K Extra"],
+]
+
+with open("../data/working_users.txt") as file:
+    working_users = eval(file.read())
+
+data = {}
+for user in working_users:
+    with open(f"../users/{user}.json") as file:
+        user_data = json.load(file)
+
+    data[user] = {}
+    for i in range(100):
+        try:
+            current_song = [
+                user_data["allScoresBest"]["mania"][i]["beatmapset"]["title"].replace("'", " ").replace('"', " "),
+                user_data["allScoresBest"]["mania"][i]["beatmap"]["version"].replace("'", " ").replace('"', " "),
+            ]
+        except IndexError:
+            break
+
+        if current_song in song_list:
+            data[user][f"{current_song[0]} [{current_song[1]}]"] = user_data["allScoresBest"]["mania"][i]["score"]
+
+with open("../data/scores.json", 'w') as file:
+    json.dump(data, file)
