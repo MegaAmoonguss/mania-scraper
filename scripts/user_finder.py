72710c14c3847bc6588740feaa07dced70d168c5
Graham Preston <graham.preston@gmail.com>
2017-04-24 11:23:34 -0400

    Read mods, fix score data

--- scripts/user_finder.py
+++ scripts/user_finder.py
@@ -2,26 +2,26 @@ import os
 import json
 
 song_list = [
-    ["Speedcore 300", "Extra"],
-    ["croiX", "GRAVITY"],
-    ["over the top", "Extra"],
-    ["Blastix Riotz", "Jinjin s INFINITE"],
-    ["HAELEQUIN", "INF"],
-    ["Bangin  Burst", "EXHAUST Lv.16"],
-    ["Verse IV", "INFINITE"],
-    ["Tokyo Teddy Bear", "SHD"],
-    ["Galaxy Collapse", "Cataclysmic Hypernova"],
-    ["M.A.M.A.", "SHD"],
-    ["Kakuzetsu Thanatos", "Isolation"],
-    ["G1ll35 d3 R415", "L45T C4LL"],
-    ["LegenD.", "KK s GRAVITY"],
-    ["C18H27NO3(extend)", "4K Capsaicin"],
-    ["BLACK or WHITE?", "DECADE vs. Usagi s INFINITE Lv.16"],
-    ["Lachryma<Re:Queen M>", "GRAVITY"],
-    ["Hesperides", "Master"],
-    ["Bokutachi no Tabi to Epilogue.[Long ver.]", "Final Voyage"],
-    ["G1ll35 d3 R415", "Shana s Extra"],
-    ["Space Time (Amane Hardcore Remix)", "Spy s 4K Extra"],
+    ["Speedcore 300", "Extra", []],
+    ["croiX", "GRAVITY", []],
+    ["over the top", "Extra", []],
+    ["Blastix Riotz", "Jinjin s INFINITE", []],
+    ["HAELEQUIN", "INF", []],
+    ["Bangin  Burst", "EXHAUST Lv.16", []],
+    ["Verse IV", "INFINITE", []],
+    ["Tokyo Teddy Bear", "SHD", []],
+    ["Galaxy Collapse", "Cataclysmic Hypernova", []],
+    ["M.A.M.A.", "SHD", []],
+    ["Kakuzetsu Thanatos", "Isolation", []],
+    ["G1ll35 d3 R415", "L45T C4LL", []],
+    ["LegenD.", "KK s GRAVITY", []],
+    ["C18H27NO3(extend)", "4K Capsaicin", []],
+    ["BLACK or WHITE?", "DECADE vs. Usagi s INFINITE Lv.16", []],
+    ["Lachryma<Re:Queen M>", "GRAVITY", []],
+    ["Hesperides", "Master", []],
+    ["Bokutachi no Tabi to Epilogue.[Long ver.]", "Final Voyage", []],
+    ["G1ll35 d3 R415", "Shana s Extra", []],
+    ["Space Time (Amane Hardcore Remix)", "Spy s 4K Extra", []],
 ]
 
 working_users = []
@@ -35,6 +35,7 @@ for file in os.listdir("../users/"):
             current_song = [
                 user_data["allScoresBest"]["mania"][i]["beatmapset"]["title"].replace("'", " ").replace('"', " "),
                 user_data["allScoresBest"]["mania"][i]["beatmap"]["version"].replace("'", " ").replace('"', " "),
+                user_data["allScoresBest"]["mania"][i]["mods"]
             ]
         except IndexError:
             break
