4fd1d55ade1ce1567d85639c3b593ff67e7582bf
Graham Preston <graham.preston@gmail.com>
2017-04-27 09:41:25 -0400

    Add best fit line and r^2

--- scripts/user_graph.py
+++ scripts/user_graph.py
@@ -1,6 +1,9 @@
 import sys
 import json
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import scipy.stats
 
 username = input("Enter user: ")
 
@@ -39,8 +42,13 @@ for song in songs:
     x.append(song[3])
     y.append(song[4] / 1000)
 
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x, y)
+x_bestfit = np.linspace(0, 11, num=50)
+y_bestfit = np.linspace(intercept, slope * 11 + intercept, num=50)
+
 plt.rc("grid", linestyle="--")
 plt.scatter(x, y)
+plt.plot(x_bestfit, y_bestfit, 'r')
 plt.axis([4.4, 7.1, 500, 1000])
 
 plt.title(f"Profile Data for {username}")
@@ -48,4 +56,7 @@ plt.xlabel("Difficulty Rating (stars)")
 plt.ylabel("Score (1000s)")
 
 plt.grid(True)
+red_patch = mpatches.Patch(color='r', label=f"Best fit line\nr^2 = {r_value**2}")
+plt.legend(handles=[red_patch])
+
 plt.show()
